b23dedf6e966ef6fac5b1ef1cded65079be2f727
Ghost <ghostx@ntlworld.com>
2016-04-27 19:13:20 +0100

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php foreach(glob('core/classes/*.php') as $class_file): require_once($class_file); endforeach; ?>
-<?php foreach(glob('core/functions/*.php') as $function_file): require_once($function_file); endforeach; ?>
-<?php if(file_exists('core/config.php')): require_once('core/config.php'); else: die('Cannot Find Configuration File'); endif; ?>
-<?php debug($config->debug); ?>
-<?php
-
-?>
